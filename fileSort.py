9b54400121e6936a2544b1be50ed14c5c2383001
AtoWithAHatto <47469051+AtoWithAHatto@users.noreply.github.com>
2023-06-19 23:26:35 +0530

    Added common file types, TODO Functionality

--- fileSort.py
+++ fileSort.py
@@ -1,34 +1,55 @@
 import shutil
 import os
+import questionary
 from time import sleep
-a = input(r'Please input a directory path: ')[1:-1]
-parent_dir = a.replace('\\', '/')
+
+a = input(r"Please input a directory path: ")[1:-1]
+parent_dir = a.replace("\\", "/")
+commonFileTypes = {
+    "jpg": "Image",
+    "jpeg": "Image",
+    "exe": "Executable",
+    "ttf": "Fonts",
+    "otf": "Fonts",
+    "zip": "Zipped",
+    "pdf": "Documents",
+    "mp3": "Audio",
+    "mp4": "Video",
+    "ogg": "Audio",
+    "wav": "Video",
+    "7z": "Zipped",
+}
 if os.path.exists(parent_dir):
+
     def CreateFolder():
         global dir_list
         dir_list = os.listdir(parent_dir)
         for i in dir_list:
-            nameL = i.split('.')
+            nameL = i.split(".")
             if len(nameL) <= 1:
                 continue
             else:
-                if nameL[1] and os.path.exists(parent_dir + '/' + nameL[1] + 'Files') == False:
-                    os.mkdir(parent_dir + '/' + nameL[1] + 'Files')
+                if (
+                    nameL[1]
+                    and os.path.exists(parent_dir + "/" + nameL[1] + "Files") == False
+                ):
+                    os.mkdir(parent_dir + "/" + nameL[1] + "Files")
 
     def SortFiles():
         global dir_list
         dir_list = os.listdir(parent_dir)
         for i in dir_list:
-            nameL = i.split('.')
+            nameL = i.split(".")
             if len(nameL) <= 1:
                 continue
             else:
                 if nameL[1]:
-                    shutil.move(f"{parent_dir}/{i}",
-                                f"{parent_dir}/{nameL[1]}Files/{i}")
+                    shutil.move(
+                        f"{parent_dir}/{i}", f"{parent_dir}/{nameL[1]}Files/{i}"
+                    )
 
     CreateFolder()
     sleep(0.5)
     SortFiles()
 else:
-    print('Please give a proper directory')
+    print("Please give a proper directory")
