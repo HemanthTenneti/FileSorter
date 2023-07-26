0cf97646320114323d47d5caf7cdd9b5163009f6
Hemanth Tenneti <hemanth10etii@gmail.com>
2023-07-26 22:47:56 +0530

    File extensions added
    Added more file extensions, need to add functionality to main file.

--- fileSort.py
+++ fileSort.py
@@ -1,24 +1,10 @@
 import shutil
 import os
-import questionary
 from time import sleep
 
 a = input(r"Please input a directory path: ")[1:-1]
 parent_dir = a.replace("\\", "/")
-commonFileTypes = {
-    "jpg": "Image",
-    "jpeg": "Image",
-    "exe": "Executable",
-    "ttf": "Fonts",
-    "otf": "Fonts",
-    "zip": "Zipped",
-    "pdf": "Documents",
-    "mp3": "Audio",
-    "mp4": "Video",
-    "ogg": "Audio",
-    "wav": "Video",
-    "7z": "Zipped",
-}
+
 if os.path.exists(parent_dir):
 
     def CreateFolder():
