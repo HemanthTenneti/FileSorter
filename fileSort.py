5ab471ad06c002f02a215d08804316db81833562
Hemanth Tenneti <hemanth10etii@gmail.com>
2023-11-11 01:28:18 +0530

    Fixed bugs & added QOL change
    Changed folders moving if they had a file extension in the name, added a button to open explorer in the selected folder.

--- fileSort.py
+++ fileSort.py
@@ -10,7 +10,7 @@ from tkinter import messagebox
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
 window = ctk.CTk()
-window.geometry("240x190")
+window.geometry("240x175")
 window.title("File Sorter")
 window.resizable(False, False)
 
@@ -19,25 +19,28 @@ def createFolder(parent_dir):
     global dir_list
     dir_list = os.listdir(parent_dir)
     for fileTypes in dir_list:
-        fileExt = fileTypes.split(".")[-1].lower()
-        if (
-            fileExt.lower() in commonFileTypes
-            and os.path.exists(parent_dir + "/" + commonFileTypes[fileExt]) == False
-        ):
-            os.mkdir(parent_dir + "/" + commonFileTypes[fileExt])
+        if not os.path.isdir(parent_dir + "\\" + fileTypes):
+            fileExt = fileTypes.split(".")[-1].lower()
+            if (
+                fileExt.lower() in commonFileTypes
+                and os.path.exists(parent_dir + "/" + commonFileTypes[fileExt]) == False
+            ):
+                os.mkdir(parent_dir + "/" + commonFileTypes[fileExt])
 
 
 def sortFiles(parent_dir):
     for fileTypes in dir_list:
-        fileExt = fileTypes.split(".")[-1].lower()
-        if fileExt.lower() in commonFileTypes:
-            shutil.move(
-                f"{parent_dir}/{fileTypes}",
-                f"{parent_dir}/{commonFileTypes[fileExt]}/{fileTypes}",
-            )
+        if not os.path.isdir(parent_dir + "\\" + fileTypes):
+            fileExt = fileTypes.split(".")[-1].lower()
+            if fileExt.lower() in commonFileTypes:
+                shutil.move(
+                    f"{parent_dir}/{fileTypes}",
+                    f"{parent_dir}/{commonFileTypes[fileExt]}/{fileTypes}",
+                )
 
 
 def sortButton():
+    global directory
     if directory not in [None, ""]:
         createFolder(directory)
         sleep(0.5)
@@ -50,7 +53,14 @@ def sortButton():
             "Something's wrong",
             "I can feel it. It was an error, you didn't specify a path",
         )
-
+        
+def exploreFolder(directory):
+    if directory == None:
+        messagebox.showwarning("Error", 'No folder selected')
+    elif os.path.isdir(directory):
+        os.startfile(directory)
+    else:
+        messagebox.showwarning("Error", 'Not a folder')
 
 directory = None
 
@@ -66,8 +76,12 @@ dirButton = ctk.CTkButton(
 sortFilesButton = ctk.CTkButton(
     master=window, command=lambda: sortButton(), text="Sort the files"
 )
-dirButton.pack(pady=30, padx=10)
-sortFilesButton.pack(pady=30, padx=20)
+explorerButton = ctk.CTkButton(
+    master=window, command=lambda: exploreFolder(directory), text="Open Folder"
+)
+dirButton.pack(pady=15, padx=15)
+sortFilesButton.pack(pady=15, padx=15)
+explorerButton.pack(pady=15, padx=15)
 
 
 window.mainloop()
